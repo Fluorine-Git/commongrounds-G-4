65fa3bd3a87b050136af2a4ab8b7951911a8ccfc
Benedict Benj Laurente <benedict.benj.laurente@student.ateneo.edu>
2026-02-23 12:46:51 +0800

    Add the necessary paths for the projected apps.

--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', )
     path('admin/', admin.site.urls),
+    path('merchstore/', include('merchstore.urls', namespace="merchstore")),
+    path('localevents/', include('localevents.urls', namespace="localevents")),
+    path('bookclub/', include('bookclub.urls', namespace="bookclub")),
+    path('diyprojects/', include('diyprojects.urls', namespace="diyprojects")),
+    path('commissions/', include('commissions.urls', namespace="commissions")),
 ]
