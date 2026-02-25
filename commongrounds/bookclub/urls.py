d8dc702a347da84e9995a941d8dcd9c38f31969e
Hiroki <hiroki.watanabe@student.ateneo.edu>
2026-02-25 19:08:04 +0800

    initialized urls.py and templates in app-level

---
+++ commongrounds/bookclub/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+# from .views import 
+
+urlpatterns = [
+#    path('books/', null, name=''),
+#    path('book/<int:pk>', null, name=''),
+]
+
+app_name = "bookclub"
