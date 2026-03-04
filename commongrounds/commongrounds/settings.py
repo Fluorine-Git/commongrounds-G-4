d83a66f4a5628fbfcd4fc2d170a4e4eea0d91d3d
Benedict Benj Laurente <benedict.benj.laurente@student.ateneo.edu>
2026-03-04 11:55:03 +0800

    Add back the anticipation lines for other apps.

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -41,7 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'merchstore'
+    'merchstore',
+
+
+
+
 ]
 
 MIDDLEWARE = [
--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -18,6 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('merchstore/', include('merchstore.urls')),
     path('admin/', admin.site.urls),
+    path('merchstore/', include('merchstore.urls')),
+
+
+
+
+
 ]
